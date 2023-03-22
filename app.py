afab6331aa6138d89206334c8296e27f79bcb027
Soham Mukherjee <iamsoham2011@gmail.com>
2023-03-22 20:34:43 +0530

    updating work,mysql integration for signup added

--- app.py
+++ app.py
@@ -1,33 +1,68 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import sqlstuff
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "ghwguiasghui0"
 sqlstuff.config()
+
+
 @app.route("/")
 def homePage():
     return render_template("index.html")
 
 @app.route("/login-signup", methods=["POST", "GET"])
-def signup():
+def login_signup():
     if request.method == "POST":
         if "login" in request.form:
-            name=request.form['username']
+            username=request.form['username']
             password = request.form['password']
-            return redirect(url_for("account", username=name, password=password))
+            session['username'] = username
+            session['password'] = password
+            return redirect(url_for("account", username=username))
         else :
-            return redirect(url_for("homePage"))
+            username = request.form['username']
+            password = request.form['password']
+            firstname = request.form['firstname']
+            lastname = request.form['lastname']
+            email = request.form['email']
+            companyname = request.form['companyname']
+            sqlstuff.signupInsert('users', username, password, firstname, lastname, email, companyname)
+            session["username"] = username
+            session["password"] = password
+            return redirect(url_for("account", username = username))
     else:
-         return render_template("login-signup.html")
-
-@app.route("/account/<username><password>")
-def account(username, password):
-    username = str(username)
-    password = str(password)
-    return "<p>" + username + "</p><p>" + password + "</p>"
+        if 'username' in session and 'password' in session:
+            username = session['username']
+            return redirect(url_for("account", username=username))
+        else:
+            return render_template("login-signup.html")
 
 @app.route("/forgot-password", methods=["POST", "GET"])
 def forgotpassword():
     if request.method == "GET":
         return render_template("forgot_password.html")
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    return redirect(url_for('login_signup'))
+
+@app.route("/account/<username>")
+def account(username):
+    if 'username' in session and 'password' in session:
+        username = str(username)
+        password = str(session['password'])
+        return "<p>" + username + "</p><p>" + password + "</p>"
+    else:
+        return redirect(url_for('login_signup'))
+    
+@app.route('/account')
+def accredirect():
+    try:
+        return redirect(url_for('account', username = session['username']))
+    except:
+        return redirect(url_for('login_signup'))
+
 
 if __name__ == "__main__":
     app.run(Debug=True)
--- sqlstuff.py
+++ sqlstuff.py
@@ -1,14 +1,46 @@
 import mysql.connector as mysql
 
-def config():
-    mydb = mysql.connect(
-    host="localhost",
-    user="root",
-    password="Soham@12"
+mydb = mysql.connect(
+        host="localhost",
+        user="root",
+        password="Soham@12"
     )
+global cursor
+cursor = mydb.cursor()
+def config():
+
+    #project-specific db checking if exists
+    cursor.execute("SHOW DATABASES")
+    i = 0
+    global dblist
+    dblist = []
+    for x in cursor:
+        global dbname
+        dbname = str(x)
+        #removing (,),' and , from the db name
+        dbname = dbname.replace('(', '')
+        dbname = dbname.replace(')', '')
+        dbname = dbname.replace(',', '')
+        dbname = dbname.replace("'", '')
+        dblist.append(dbname)
+        i = i + 1
+    if 'main' in dblist:
+        cursor.execute("USE main")
+    else:
+        cursor.execute("CREATE DATABASE main;")
+        cursor.execute("USE main")
+        cursor.execute("CREATE TABLE user(username VARCHAR(255) PRIMARY KEY, email VARCHAR(255), password VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255), companyname VARCHAR(255));")
+    print('config done')
+
+def showall(table):
     global cursor
-    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM " + table)
+    dblist = cursor.fetchall()
+    return dblist
 
-
-
+def signupInsert(tablename, username, password, firstname, lastname, email, companyname):
+    sql = "INSERT INTO " + tablename + "(username, email, password, firstname, lastname, companyname)" + "VALUES (%s, %s, %s, %s, %s, %s);"
+    val = (username, email, password, firstname, lastname, companyname)
+    cursor.execute(sql, val)
+    mydb.commit()
 
